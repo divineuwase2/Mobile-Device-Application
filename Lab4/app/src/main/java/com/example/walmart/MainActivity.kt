7be492ae214c2050a502b995c3a1428565fd6df9
kdduwase <74629502+kdduwase@users.noreply.github.com>
2022-03-07 18:50:49 -0600

    adding lab4

---
+++ Lab4/app/src/main/java/com/example/walmart/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.walmart
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+     var users= ArrayList<User>()
+    var correctDetailsFlag=false
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val user=User("Divine", "Uwase", "duwase@gmail.com", "1234")
+        val user1 = User("Harry", "Poter", "hpoter@gmail.com", "2345")
+        val user2 = User("Michelle", "Celine", "mceline@gmail.com", "3456")
+        val user3 = User("Trevor", "Noah", "tnoah@gmail.com", "4567")
+        val user4 = User("Mary", "Grace", "mgrace@gmail.com", "5678")
+        val user5 = User("Nicholas", "Katende", "nkatende@gmail.com", "6789")
+        users.add(user1)
+        users.add(user2)
+        users.add(user3)
+        users.add(user4)
+        users.add(user5)
+
+    }
+    fun login(view:View) {
+        for (i in users) {
+            val uname: String = i.userName
+            val upassword: String = i.password
+            if (et1.text.toString().equals(uname) && et2.text.toString().equals(upassword)) {
+                correctDetailsFlag = true
+
+                    var intent = Intent(this, ShopByCategory::class.java)
+                    intent.putExtra("user", uname)
+                    startActivity(intent)
+                    break
+
+            }
+
+        }
+    }
+
+    fun createAccount(view: View){
+        val intent = Intent(this,Register::class.java)
+        startActivity(intent)
+
+    }
+    @Suppress("DEPRECATION")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==1){
+            if(resultCode == Activity.RESULT_OK){
+                if(data != null){
+                    val ReturnedResult = data.getSerializableExtra("newUser") as User
+                    users.add(ReturnedResult)
+                    Toast.makeText(this, "Account created successfully with user name of ${ReturnedResult.userName}", Toast.LENGTH_LONG).show()
+                }
+
+            }
+        }
+    }
+
+    fun forgetPassword(view: View){
+        var userName = et1.text.toString()
+        var password = et2.text.toString()
+
+        if(!userName.equals("") && !password.equals("")){
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra("Password", password)
+            intent.type = "text/plain"
+            startActivity((Intent.createChooser(intent, "Password sent to your email address")))
+        }  else {
+            Toast.makeText(this, "Incorrect email or password", Toast.LENGTH_SHORT).show()
+        }
+    }
+    }
+
